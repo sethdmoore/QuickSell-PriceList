4bff23b519bd9aa68df4bc3e8e80c7824c67a6d2
Seth Moore <sethdmoore@github.com>
2015-01-19 15:07:14 -0500

    Value checking (price 0 = non-sellable). Soft tabs > hard tabs

--- src/com/mcapplbee/pricelist/PriceList.java
+++ src/com/mcapplbee/pricelist/PriceList.java
@@ -14,117 +14,128 @@ import java.util.Set;
 
 
 public class PriceList extends JavaPlugin {
-	private String combined_args = new String("");
-	private Configuration config;
-	private Map<String, Object> priceList;
+    private String combined_args = new String("");
+    private Configuration config;
+    private Map<String, Object> priceList;
 
-	
-	@Override
-	public void onEnable() {
-		readQSConfig();
-		getLogger().info("Loaded");
-	}
+    
+    @Override
+    public void onEnable() {
+        readQSConfig();
+        getLogger().info("Loaded");
+    }
 
 
-	@Override
-	public void onDisable() {
-		getLogger().info("Unloaded");
-	}
+    @Override
+    public void onDisable() {
+        getLogger().info("Unloaded");
+    }
 
-	
-	public void readQSConfig() {
-		try {
-			getLogger().info("Trying to grab QuickSell config...");
-			config = getServer().getPluginManager().getPlugin("QuickSell").getConfig();
-			//Configuration config = pc.getConfig();
-			//config = qs.getConfig();
+    
+    public void readQSConfig() {
+        try {
+            config = getServer().getPluginManager().getPlugin("QuickSell").getConfig();
 
-			priceList = config.getConfigurationSection("shops.global.price").getValues(false);
-			getLogger().info("Grabbed the config!");
+            priceList = config.getConfigurationSection("shops.global.price").getValues(false);
+            getLogger().info("Successfully loaded QuickSell config");
 
-			Set set = priceList.entrySet();
-			Iterator i = set.iterator();
+            Set set = priceList.entrySet();
+            Iterator i = set.iterator();
 
-			while (i.hasNext()) {
-				Map.Entry me = (Map.Entry)i.next();
-				//getLogger().info(me.getKey() + ": " + me.getValue());
-				//getLogger().info(BeanUtils.describe(bean));
-			}
+            while (i.hasNext()) {
+                Map.Entry me = (Map.Entry)i.next();
+            }
 
-		} catch (NullPointerException e) {
-			getLogger().info("ERROR: Exception grabbing a config");
-			getLogger().info("ERROR: Make sure QuickSell is enabled, and working");
-			getLogger().info("ERROR: Make sure shops.global is defined, and at"
-							 + " least one item listed under price");
-		} finally {
-		}
-	}
-	
+        } catch (NullPointerException e) {
+            getLogger().info("ERROR: Exception grabbing a config");
+            getLogger().info("ERROR: Make sure QuickSell is enabled, and working");
+            getLogger().info("ERROR: Make sure shops.global is defined, and at"
+                             + " least one item listed under price");
+        } finally {
+        }
+    }
+    
 
-	public ArrayList<String> searchPrices(String searchterm) {
-		ArrayList<String> results = new ArrayList<String>();
+    public ArrayList<String> searchPrices(String searchterm) {
+        ArrayList<String> results = new ArrayList<String>();
+        String value = new String();
 
-		Set set = priceList.entrySet();
-		Iterator i = set.iterator();
+        Set set = priceList.entrySet();
+        Iterator i = set.iterator();
 
-		while (i.hasNext()) {
-			Map.Entry me = (Map.Entry)i.next();
-			String candidate = me.getKey().toString();
-			
-			if (candidate.contains(searchterm.toUpperCase())) {
-				candidate = WordUtils.capitalize(
-								candidate.replace("_",  " ").toLowerCase());
-				results.add(ChatColor.GRAY + candidate + ": " +
-							ChatColor.GOLD + me.getValue());
-			}
-		}
-		return results;
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("pricelist")) {
-			if (args.length == 0) {
-				printUsage(sender);
-				return false;
-			}
+        while (i.hasNext()) {
+            Map.Entry me = (Map.Entry)i.next();
+            String candidate = me.getKey().toString();
+            
+            value = me.getValue().toString();
+            
+            if (value.equals("0.0")) {
+                value = ChatColor.RED + "No Value (Do not sell!)";
+            } else {
+                value = ChatColor.GOLD + value;
+            }
+            
+            if (candidate.contains(searchterm.toUpperCase())) {
+                candidate = WordUtils.capitalize(
+                                candidate.replace("_",  " ").toLowerCase());
+                results.add(ChatColor.GRAY + candidate + ": " + value);
+            }
+        }
+        return results;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("pricelist")) {
+            // Input validation
+            if (args.length == 0) {
+                printUsage(sender);
+                return true;
+            }
 
-			combined_args = StringUtils.join(args, "_");
-			searchPrices(combined_args);
-			
-			ArrayList<String> results = searchPrices(combined_args);
-			
-			if (results.size() > 0) {
-				sender.sendMessage(ChatColor.YELLOW + "PriceList" + 
-								   ChatColor.WHITE + " results ---");
-					for (String s : results) {
-						sender.sendMessage(s);
-					} 
-			} else {
-				sender.sendMessage("PriceList: No results for " + StringUtils.join(args, " "));
-			}
-			
-			try {
-				//findBlocks = Material.matchMaterial(combined_args).toString();	
-				
-				//Material.matchMaterial(combined_args).toString();
-			} catch (NullPointerException e) {
-				sender.sendMessage("Error: Couldn't find " +
-				    combined_args.replace("_",  " "));
-				printUsage(sender);
-			} finally {
-				
-			}
 
-			//getLogger().info("Command Issued");
-			return true;
-		}
-		return false;
-	}
+            // Match the format of keys in the Material enum
+            combined_args = StringUtils.join(args, "_");
 
-	public void printUsage(CommandSender sender) {
-		sender.sendMessage(ChatColor.YELLOW + "Pricelist: " + 
-						   ChatColor.GRAY + "Usage is /pricelist <item>");
-		sender.sendMessage("EG: /pricelist stone");
-	}
+            if (combined_args.length() < 3) {
+                sender.sendMessage(ChatColor.YELLOW + "PriceList" + ChatColor.WHITE + 
+                                   ": Please enter at least 3 characters");
+                return true;
+            }
+            
+            searchPrices(combined_args);
+            
+            ArrayList<String> results = searchPrices(combined_args);
+            
+            if (results.size() > 0) {
+                sender.sendMessage(ChatColor.YELLOW + "PriceList" + 
+                                   ChatColor.WHITE + " results ---");
+                    for (String s : results) {
+                        sender.sendMessage(s);
+                    } 
+            } else {
+                sender.sendMessage("PriceList: No results for " + StringUtils.join(args, " "));
+            }
+            
+            try {
+                //findBlocks = Material.matchMaterial(combined_args).toString();    
+                
+                //Material.matchMaterial(combined_args).toString();
+            } catch (NullPointerException e) {
+                sender.sendMessage("Error: Couldn't find " +
+                    combined_args.replace("_",  " "));
+                printUsage(sender);
+            } finally {
+                
+            }
+            return true;
+        }
+        return true;
+    }
+
+    public void printUsage(CommandSender sender) {
+        sender.sendMessage(ChatColor.YELLOW + "Pricelist: " + 
+                           ChatColor.GRAY + "Usage is /pricelist <item>");
+        sender.sendMessage("EG: /pricelist stone");
+    }
 }
